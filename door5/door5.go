c4397e0bfd659399fc2ac224bddd8ec8f459c6be
viet duc do <duc.do32@mailbox.org>
2025-01-26 20:57:32 +0100

    door5: Read raw rows of rules and logs
    Scan each line.
    Before encounting empty line append row to buffer of rules.
    Then set a flag to append rows to buffer of logs.

--- door5/door5.go
+++ door5/door5.go
@@ -1,9 +1,47 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+)
+
+type (
+	rowRuleData string
+	rowLogData  string
 )
 
 func main() {
 	fmt.Println("Solving door 5...")
+
+	// Buffer for rules and logs
+	bufRules := make([]rowRuleData, 0, 100)
+	bufLogs := make([]rowLogData, 0, 20)
+
+	f, err := os.Open("data/small.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	flagScanRules := true
+	for scanner.Scan() {
+
+		row := scanner.Text()
+
+		if len(row) == 0 {
+			flagScanRules = !flagScanRules
+			continue
+		}
+
+		if flagScanRules {
+			bufRules = append(bufRules, rowRuleData(row))
+		} else {
+			bufLogs = append(bufLogs, rowLogData(row))
+		}
+	}
+
+	fmt.Printf("Rows of rules: %d\n"+"Rows of logs: %d\n", len(bufRules), len(bufLogs))
 }
